e6b9d0c6c4c7fc346c41d668f320ac17c25567e0
RinMaru01 <leanmarforrosuelo01@gmail.com>
2023-04-11 19:24:40 +0800

    index

--- LeanPHP/index.php
+++ LeanPHP/index.php
@@ -1,24 +1,26 @@
-<?php require 'includes/header.php' ?>
+<?php require_once 'includes/header.php' ?>
 
 <div class="container"> 
-    <form>
+    <form  method="GET" action="action_page.php">
+
     <div class="mb-3">
-        <label for="exampleInputFirstName1" class="form-label">First Name</label>
-        <input type="firstname" class="form-control" id="exampleInputFirstName1" placeholder="First Name">
-    </div><div class="mb-3">
+        <label for="firstname" class="form-label">First Name</label>
+        <input required type="text" class="form-control" id="firstname" placeholder="First Name" name="firstname">
+    </div>
+    <div class="mb-3">
         <label for="exampleInputLastName1" class="form-label">Last Name</label>
-        <input type="lastname" class="form-control" id="exampleInputLastName1" placeholder="Last Name">
+        <input type="lastname" class="form-control" id="exampleInputLastName1" placeholder="Last Name" name="lname">
     </div>
 
     <div class="mb-3">
         <!-- <label for="datepicker"></label> -->
         <label for="birthdate" class="form-label">Date of Birth</label>
-        <input type="datepicker" class="form-control" id="datepicker" placeholder="mm/dd/yyyy">
+        <input type="datepicker" class="form-control" id="datepicker" placeholder="mm/dd/yyyy" name="birthdate">
   
     <!-- <p>Date: <input type="text" id="datepicker"></p> -->
     <div class="mb-3">
         <label for="exampleInputSpecialty" class="form-label">Specialty</label>
-        <select class="form-select" aria-label="Default select example">
+        <select class="form-select" aria-label="Default select example" name="jobs">
         <option selected>Specialty</option>
         <option value="1">Database Admin</option>
         <option value="2">Software Developer</option>
@@ -27,11 +29,11 @@
     </div>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email">
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email" name="email">
     </div>
     <div class="mb-3">
         <label for="exampleInputContact1" class="form-label">Contact Number</label>
-        <input type="contact" class="form-control" id="exampleInputContact1" placeholder="Enter Contact Number">
+        <input type="contact" class="form-control" id="exampleInputContact1" placeholder="Enter Contact Number" name="contactnum">
     </div>
     
    
@@ -40,4 +42,4 @@
 </div>
 
  
-<?php require 'includes/footer.php' ?>
+<?php require_once 'includes/footer.php' ?>

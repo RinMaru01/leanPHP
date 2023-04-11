369ea732ed7cd96531b401c87643f0903abf0bcf
RinMaru01 <91763687+RinMaru01@users.noreply.github.com>
2023-04-11 18:00:02 +0800

    Update footer.php

--- LeanPHP/includes/footer.php
+++ LeanPHP/includes/footer.php
@@ -2,9 +2,17 @@
     <?php echo"<p class=text-center> @Copyright &copy; 2021-". date("Y") . " WEB DEVELOPMENT II Attendance"?>
 </div>
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
   <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+  } );
+  
+  </script>
   </body>
 </html>
